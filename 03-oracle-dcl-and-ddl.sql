86753965859331484c37ad9e88f0083ec53df610
Blt24Q1 <blt24q1@gmail.com>
2024-05-02 15:21:53 +0900

    Update 03-oracle-dcl-and-ddl.sql

--- 03-oracle-dcl-and-ddl.sql
+++ 03-oracle-dcl-and-ddl.sql
@@ -158,3 +158,72 @@ ADD CONSTRAINT fk_author_id
 SELECT * FROM DICTIONARY;
         
 -- 사용자 스키마 객체 : USER_OBJECTS
+SELECT * FROM USER_OBJECTS;
+-- 사용자 스키마의 이름과 타입 정보 출력
+SELECT OBJECT_NAME, OBJECT_TYPE FROM USER_OBJECTS;
+
+-- 제약 조건의 확인
+SELECT * FROM USER_CONSTRAINTS;
+SELECT CONSTRAINT_NAME,
+    CONSTRAINT_TYPE,
+    SEARCH_CONDITION,
+    TABLE_NAME
+FROM USER_CONSTRAINTS;
+
+-- BOOK 테이블에 적용된 제약조건의 확인
+SELECT CONSTRAINT_NAME,
+    CONSTRAINT_TYPE,
+    SEARCH_CONDITION
+FROM USER_CONSTRAINTS
+WHERE TABLE_NAME='BOOK';
+
+TRUNCATE TABLE author;
+
+-- INSERT : 테이블에 새 레코트(튜플) 추가
+-- 제공된 컬럼 목록의 순서와 타입, 값 목록의 순서와 타입이 일치해야 함
+-- 컬럼 목록을 제공하지 않으면 테이블 생성시 정의된 컬럼의 순서의 타입을 따른다
+
+-- 컬럼 목록이 제시되지 않았을 때
+INSERT INTO author
+VALUES(1, '박경리', '토지 작가');
+
+SELECT * FROM author;
+
+-- 컬럼 목록을 제시했을 때,
+--  제시한 컬럼의 순서와 타입대로 값 목록을 제공해야 함
+INSERT INTO author (author_id, author_name)
+VALUES(2, '김영하');
+
+SELECT * FROM author;
+
+-- 컬럼 목록을 제공했을 때,
+-- 테이블 생성시 정의된 컬럼의 순서와 상관 없이 데이터 제공 가능
+INSERT INTO author (author_name, author_id, author_desc)
+VALUES('류츠신', 3, '삼체 작가');
+
+SELECT * FROM author;
+--desc author;
+--ALTER TABLE author RENAME COLUMN ANTHOR_NAME TO author_name;
+--DESC author;
+
+ROLLBACK; -- 반영 취소
+
+SELECT * FROM author;
+
+INSERT INTO author
+VALUES(1, '박경리', '토지 작가');
+INSERT INTO author (author_id, author_name)
+VALUES(2, '김영하');
+INSERT INTO author (author_name, author_id, author_desc)
+VALUES('류츠신', 3, '삼체 작가');
+
+SELECT * FROM author;
+
+COMMIT;     --  변경사항 반영
+
+SELECT * FROM author;
+
+-- UPDATE
+-- 특정 레코드의 컬럼 값을 변경한다
+-- WHERE 절이 없으면 모든 레코드가 변경
+-- 가급적 WHERE 절로 변경하고자 하는 레코드를 지정하도록 함
